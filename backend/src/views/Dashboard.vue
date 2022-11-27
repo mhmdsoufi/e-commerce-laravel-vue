22dcca6a6ab40ea2331972ad2e3d60d2744c019c
mhmdsoufi <msofi.1999@gmail.com>
2022-11-27 11:28:43 +0300

    vue_backend

---
+++ backend/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Dashboard form</h1>
+</template>
+
+<script>
+export default {
+    name:"dashboard"
+}
+</script>
+
+<style>
+
+</style>
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::get('/user', [\App\Http\Controllers\Api\AuthController::class, 'getUser']);
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+
+    //product
+    Route::apiResource('products', ProductController::class);
+
+});
+
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
